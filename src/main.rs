deeee1de722bf7c725c4cc4bf8aac9aae76530c8
maikimilk <maimu.ltd@gmail.com>
2021-05-25 18:13:26 +0900

    乱数を取得・表示

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("数当てゲーム");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("シークレットナンバーはこれ: {}", secret_number);
 
     println!("どの数だとおもう？ = ");
 
@@ -12,5 +17,4 @@ fn main() {
         .expect("読み込み失敗");
 
     println!("入力値: {}", guess);
-
 }
