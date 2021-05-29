c2bc4e8488592275f6d410ac9b95ff33691f86e1
maikimilk <maimu.ltd@gmail.com>
2021-05-29 17:45:54 +0900

    文字入力への対応を追加

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
-
         println!("どの数だとおもう？ = ");
 
         let mut guess = String::new();
@@ -17,11 +16,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("読み込み失敗");
 
-        let guess:u32 = match guess.trim().parse(){
-            Ok(num) => num,
-            Err(_)=> continue,
-        };
-
+        let guess= guess
+            .trim()
+            .parse::<u32>();
+        match guess
+            {
+                Ok(_) => {},
+                Err(_)=> {println!("数を入力してください");continue;}
+            };
+        let guess =  guess.unwrap();
         println!("入力値: {}", guess);
 
         match guess.cmp(&secret_number){
